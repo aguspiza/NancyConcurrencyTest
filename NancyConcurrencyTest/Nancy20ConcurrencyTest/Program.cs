5c5ddcaedcbb6701114d8bfe74c981521535977b
agustin <agustin@by.com.es>
2017-07-02 19:50:23 +0200

    initial commit

---
+++ NancyConcurrencyTest/Nancy20ConcurrencyTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Nancy20ConcurrencyTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Nancy 2.0!");
+			var nancyhost = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:3333"));
+			nancyhost.Start();
+            Console.ReadLine();
+        }
+    }
+}
+
+public class NancyConcurrencyTestModule : NancyModule {
+    public NancyConcurrencyTestModule()
+    {
+        //ping
+        Get("/async", async (t, ct) => {
+            await Task.Delay(5000);
+            return "Hello world!";
+        });
+    }
+}
---
+++ NancyConcurrencyTest/NancyConcurrencyTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace NancyConcurrencyTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Nancy 1.4!");
+			var nancyhost = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:3333"));
+			nancyhost.Start();
+            Console.ReadLine();
+        }
+    }
+}
+
+public class NancyConcurrencyTestModule : NancyModule
+{
+	public NancyConcurrencyTestModule()
+	{
+		//ping
+		Get["/async", true] = async (t, ct) =>
+				{
+					await Task.Delay(5000);
+					return "Hello World!";
+				};
+	}
+}
